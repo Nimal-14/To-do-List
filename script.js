9c82d6b4baa3a8404ca17ec11922ef03a9319e0a
Nimal-14 <antonimal2005@gmail.com>
2026-08-28 13:53:03 +0530

    Added complete and delete button

--- script.js
+++ script.js
@@ -25,11 +25,42 @@ function AddTask() {
     taskItem.classList.add("task-item");
 
     const taskTextElement = document.createElement("span");
+    taskTextElement.classList.add("task-text");
     taskTextElement.textContent = taskText;
 
+    const taskActions=document.createElement("div");
+    taskActions.classList.add("task-actions");
+
+    const completeButton = document.createElement("button");
+    completeButton.classList.add("complete-button");
+    completeButton.textContent = "Complete";
+
+    const deleteButton =document.createElement("button");
+    deleteButton.classList.add("delete-button");
+    deleteButton.textContent = "Delete";
+
+    completeButton.addEventListener('click',function(){
+        taskItem.classList.toggle('completed');
+
+        if(taskItem.classList.contains('completed')){
+            completeButton.textContent = 'Undo';
+        } else {
+            completeButton.textContent = 'Complete';
+        }
+    });
+
+    deleteButton.addEventListener('click',function(){
+        taskItem.remove();
+    })
+
+    taskActions.appendChild(completeButton);
+    taskActions.appendChild(deleteButton);
+
     taskItem.appendChild(taskTextElement);
+    taskItem.appendChild(taskActions);
+
     taskList.appendChild(taskItem);
     
     taskInput.value = '';
-    task.focus();
+    taskInput.focus();
 }
